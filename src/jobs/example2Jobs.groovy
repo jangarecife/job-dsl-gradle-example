82ccbddd479c45b13e88b40e63d97478888c6ad7
JangeRecife <jangarecife@gmail.com>
2018-01-03 08:21:55 +0100

    Changed repo to J..R..

--- src/jobs/example2Jobs.groovy
+++ src/jobs/example2Jobs.groovy
@@ -1,7 +1,8 @@
 import groovy.json.JsonSlurper
 
 String basePath = 'example2'
-String repo = 'sheehan/grails-example'
+String repo = 'jangarecife/job-dsl-gradle-example'
+
 
 folder(basePath) {
     description 'This example shows how to create a set of jobs for each github branch, each in its own folder.'
